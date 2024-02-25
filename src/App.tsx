85286a65c7f8ff4762f083725c863dc01b290026
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-02-25 22:32:57 +0200

    Rating created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating as R} from "./components/rating/Rating";
 
 function App() {
+    console.log("App rendering");
     return (
         <div>
+            <R/>
+            <Accordion/>
+            <R/>
         </div>
     );
 }
+
 
 export default App;
